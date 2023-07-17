31d4f04886931201728c467d266f5c375146a6d8
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-07-17 10:26:45 -0300

    Initial commit

---
+++ src/main/java/com/patrickmartins/aula01/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.patrickmartins.aula01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String index(Model model) {
+		model.addAttribute("msnBemVindo", "Bem-vindo à biblioteca");
+		return "publica-index";
+	}
+	
+}
