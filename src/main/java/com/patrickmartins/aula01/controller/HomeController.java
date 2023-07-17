059c92509a3b63f4a028e5a7e7bb8c1189f4ae80
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-07-17 12:48:33 -0300

    Alterações básicas na estrutura do código.

--- src/main/java/com/patrickmartins/aula01/controller/HomeController.java
+++ src/main/java/com/patrickmartins/aula01/controller/HomeController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/") 
 	public String index(Model model) {
 		model.addAttribute("msnBemVindo", "Bem-vindo à biblioteca");
 		return "publica-index";
 	}
-	
+
 }
