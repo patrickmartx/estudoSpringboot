2b7fc6af4e69d7c81261a1f660d0af865da93cfd
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-07-18 11:18:04 -0300

    Aula 04: Interface que herda os métodos do JpaRepository, fazendo a comunicação com o banco de dados.

---
+++ src/main/java/com/patrickmartins/aula01/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.patrickmartins.aula01.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.patrickmartins.aula01.modelo.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
+
+
