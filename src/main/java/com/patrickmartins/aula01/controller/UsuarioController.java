ca6a7700aea55ccc4f67007a9d44b624c2697072
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-07-18 15:50:33 -0300

    Aula 07: adicionado controlador para deletar algum cadastro no banco de dados.

--- src/main/java/com/patrickmartins/aula01/controller/UsuarioController.java
+++ src/main/java/com/patrickmartins/aula01/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -44,4 +45,13 @@ public class UsuarioController {
 		model.addAttribute("usuarios", usuarioRepository.findAll());
 		return "/auth/admin/admin-listar-usuario";
 	}
+	
+	@GetMapping("/admin/apagar/{id}")
+	public String deleteUser(@PathVariable("id") long id, Model model) {
+		Usuario usuario = usuarioRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Id inválido:" + id));
+		usuarioRepository.delete(usuario);
+	    return "redirect:/usuario/admin/listar";
+	}
+
 }
