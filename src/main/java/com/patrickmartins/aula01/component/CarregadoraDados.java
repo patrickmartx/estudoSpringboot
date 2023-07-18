ad4ce9b0e31221d7b8c5b775e4151408a0e3a661
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-07-18 17:22:35 -0300

    Aula 09: Classe cuja função é pré-definir os papéis disponíveis no sistema.

---
+++ src/main/java/com/patrickmartins/aula01/component/CarregadoraDados.java
@@ -0,0 +1,31 @@
+package com.patrickmartins.aula01.component;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.patrickmartins.aula01.modelo.Papel;
+import com.patrickmartins.aula01.repository.PapelRepository;
+
+@Component
+public class CarregadoraDados implements CommandLineRunner {
+
+	@Autowired
+	private PapelRepository papelRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		String[] papeis = {"ADMIN", "USER", "BIBLIOTECARIO"};
+		
+		for (String papelString: papeis) {
+			Papel papel = papelRepository.findByPapel(papelString);
+			if (papel == null) {
+				papel = new Papel(papelString);
+				papelRepository.save(papel);
+			}
+		}				
+	}
+
+
+}
