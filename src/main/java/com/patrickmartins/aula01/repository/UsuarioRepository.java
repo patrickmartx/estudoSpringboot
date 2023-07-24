a8f2996e799a8842e6f4782ef5889c69302f419e
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-07-24 17:37:51 -0300

    Aula 10: Adicionado função que permite verificar se usuário já existe no banco de dados.

--- src/main/java/com/patrickmartins/aula01/repository/UsuarioRepository.java
+++ src/main/java/com/patrickmartins/aula01/repository/UsuarioRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.patrickmartins.aula01.modelo.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+	Usuario findByLogin(String login);
 }
 
 
